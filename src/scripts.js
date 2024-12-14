cd2fe2d6a0c03a4c51b280f80216badf3b8536d7
Anh Tuan Bui <anhtuan128@gmail.com>
2024-12-14 15:46:42 +1100

    add web view

---
+++ src/scripts.js
@@ -0,0 +1,30 @@
+import {
+  revenue,
+  expenses,
+  grossProfitMargin,
+  netProfitMargin,
+  workingCapitalRatio,
+} from "../lib/functions.js";
+
+import { formatCurrency, formatPercentage } from "../lib/utils.js";
+
+fetch("data/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const jsonData = data.data;
+    document.getElementById("revenue").innerHTML = formatCurrency(
+      revenue(jsonData)
+    );
+    document.getElementById("expenses").innerHTML = formatCurrency(
+      expenses(jsonData)
+    );
+    document.getElementById("grossProfitMargin").innerHTML = formatPercentage(
+      grossProfitMargin(jsonData)
+    );
+    document.getElementById("netProfitMargin").innerHTML = formatPercentage(
+      netProfitMargin(jsonData)
+    );
+    document.getElementById("workingCapitalRatio").innerHTML = formatPercentage(
+      workingCapitalRatio(jsonData)
+    );
+  });
